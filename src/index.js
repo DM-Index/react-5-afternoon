9bfb30d0cd366c2c517f1a5a9b26298e2ac46add
Nathan <n.hatz@yahoo.com>
2018-05-21 14:28:24 -0500

    Half-way Finished

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import "./index.css";
 
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
+  document.getElementById("root")
+);
